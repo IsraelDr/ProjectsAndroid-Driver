d1e8fb05ef1fa1ac97a767f9ceec61f8bd530bd8
Israel <Israel@github.com>
2018-12-20 18:16:55 +0200

    AUTOMATIC update

--- app/src/main/java/com/example/srulispc/projectsandroid_driver/controller/model/datasource/FireBase.java
+++ app/src/main/java/com/example/srulispc/projectsandroid_driver/controller/model/datasource/FireBase.java
@@ -1,11 +1,13 @@
 package com.example.srulispc.projectsandroid_driver.controller.model.datasource;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.example.srulispc.projectsandroid_driver.controller.model.backend.Ibackend;
 import com.example.srulispc.projectsandroid_driver.controller.model.entities.Driver;
 import com.example.srulispc.projectsandroid_driver.controller.model.entities.Ride;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,6 +31,33 @@ public class FireBase implements Ibackend {
 
     public FireBase() {
         database = FirebaseDatabase.getInstance();
+        myRef=database.getReference("Rides");
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                //rideList.put(dataSnapshot.getKey(),dataSnapshot.getValue(Ride.class));
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         //-------------------------Rides Listener---------------------------
 //        myRef = database.getReference("Rides");
 //        myRef.addValueEventListener(new ValueEventListener() {
